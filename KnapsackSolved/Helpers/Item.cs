e46da75ad9a812c6f3f75b1eca35c78e6f7fb65a
Jhonatan Scremim <jhonatanscremim@gmail.com>
2021-04-17 18:01:25 -0300

    feat(create-project)

---
+++ KnapsackSolved/Helpers/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnapsackSolved.Helpers
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ KnapsackSolved/Helpers/Knapsack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnapsackSolved.Helpers
+{
+    public class Knapsack
+    {
+        public int MaxCost { get; set; }
+        public int PopSize { get; set; }
+        public double CrossoverProb { get; set; }
+        public double MutationProb { get; set; }
+        public int Terminate { get; set; }
+        public int Generation { get; set; }
+        public List<Item> Items { get; set; }
+
+        public Knapsack(int maxCost, int popSize, double crossoverProb, double mutationProb, int terminate, int generation, List<Item> items)
+        {
+            MaxCost = maxCost;
+            PopSize = popSize;
+            CrossoverProb = crossoverProb;
+            MutationProb = mutationProb;
+            Terminate = terminate;
+            Generation = generation;
+            Items = items;
+        }
+
+        public int Solver()
+        {
+            return MaxCost;
+        }
+    }
+}
